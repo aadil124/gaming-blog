4738af049fac6563a866012b4b02e61353f1db62
aadil124 <ansariaadil124@gmail.com>
2023-07-11 20:51:51 +0530

    updated app files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AllBlogs from "./components/AllBlogs";
 import AboutUs from "./components/AboutUs";
 import Teams from "./components/Teams";
 
-function App() {
-  const subscriber = 1000;
-  const blogTitle = "Best Games of 2023";
-  const blogLikes = 100;
-  const blogLink = "https://letsupgrade.in/programs";
-  const blogLink2 = "https://letsupgrade.in/programs;";
 
-  const author = { nameAuthor: "Sumit", age: 23, blogWritten: 20 };
+function App() {
+  // const subscriber = 1000;
+  // const blogTitle = "Best Games of 2023";
+  // const blogLikes = 100;
+  // const blogLink = "https://letsupgrade.in/programs";
+  // const blogLink2 = "https://letsupgrade.in/programs;";
+
+  // const author = { nameAuthor: "Sumit", age: 23, blogWritten: 20 };
 
   return (
     <BrowserRouter>
