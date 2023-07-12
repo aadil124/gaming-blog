3913e77c9f4eda1c378e247a4be9355355587d4b
aadil124 <ansariaadil124@gmail.com>
2023-07-12 11:37:21 +0530

    added home component

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+const Homepage = () => {
+  return (
+    <div className="home">
+      <h1>Welcome To Gaming Community</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur velit
+        laudantium, unde exercitationem incidunt tempore esse neque quo itaque
+        delectus numquam fugit. Quisquam, dolorum ab. Provident voluptates
+        beatae dolorem neque.
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;
