f354452e2fc0262881a3b289d3fa6bed03516449
aadil124 <ansariaadil124@gmail.com>
2023-07-12 11:38:04 +0530

    added teams component

---
+++ src/components/Teams.js
@@ -0,0 +1,10 @@
+const Teams = () => {
+    return (
+        <div className="team">
+            <h1>Meet out Teams</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, ducimus voluptate ratione perspiciatis quaerat deleniti molestiae aspernatur. Laboriosam eius perspiciatis magnam veritatis nulla. Adipisci magnam non accusamus consequatur eaque amet!</p>
+        </div>
+    );
+}
+
+export default Teams;
