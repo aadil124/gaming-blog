2b8da906ea5c4411efa0a3fa07e07180d8c35349
aadil124 <ansariaadil124@gmail.com>
2023-07-12 11:36:23 +0530

    added about us component

---
+++ src/components/AboutUs.js
@@ -0,0 +1,15 @@
+const AboutUs = () => {
+  return (
+    <div className="AboutUs">
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+        voluptatibus possimus officiis, laboriosam beatae expedita ex, quod
+        eligendi, nostrum nam ipsa aliquam aperiam amet. Ipsam iusto minus
+        adipisci necessitatibus officiis!
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
